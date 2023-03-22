bd57f0ffa5a0e933c6aeb1c963d5de9b8ba7b38b
Udit Kumar Agrawal <2001uditagrawal@gamil.com>
2023-03-22 21:19:48 +0530

    1.py

---
+++ 1.py
@@ -0,0 +1,10 @@
+print("my first program")
+a=10 ;print(a)
+n=input("enter a no.")
+print(n)
+print("hello",n)
+print("enter th cost of produxt")
+c=input("cost")
+d=int(c)
+answer=d*0.9
+print(answer)
